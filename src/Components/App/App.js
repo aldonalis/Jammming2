d217f069c6c2d4679aa49b8ffafe63b999e0267b
Aldona <aldona.lis.1997@gmail.com>
2018-01-03 19:01:53 +0000

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: 'a', artist: 'a', album: 'a', id: 1},
+        {name: 'a', artist: 'a', album: 'a', id: 2},
+        {name: 'a', artist: 'a', album: 'a', id: 3}
+      ],
+      playlistName: 'Ada Playlist',
+      playlistTracks: [
+        {name: 'b', artist: 'b', album: 'b', id: 4},
+        {name: 'b', artist: 'b', album: 'b', id: 5},
+        {name: 'b', artist: 'b', album: 'b', id: 6}
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+  } 
+
+  addTrack(track) {
+    if ({this.state.searchResults.id} !== {this.state.playlistTracks.id}) {
+      return this.state.playlistTracks.push(track)
+    }
+    this.setState({this.playlistTracks = this.playlistTracks})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks && this.props.tracks.map((track, id) => {
+          return (
+            <Track
+              key={id}
+              track={track}
+              isRemoval={true}
+              onAdd={this.props.onAdd} 
+            />
+          );
+        })}
+      </div>
+    )
+  }
+}
+
+export default TrackList;
